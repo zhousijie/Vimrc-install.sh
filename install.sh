6561cbc347a5e3b603a84940e2d13b809a3de3f1
Zhou Sijie <zhousijie@pku.edu.cn>
2017-06-19 20:28:44 +0800

    A installfile && vimrc

---
+++ install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+sudo apt-get update 
+sudo apt-get install vim
+sudo apt-get install cmake 
+sudo apt-get install automake
+sudo apt-get install autoconf
+sudo apt-get install libboost-dbg
+sudo apt-get install libboost-all-dev
+sudo apt-get install mpi
+sudo apt-get install gfortran
+sudo apt-get install git
+sudo apt-get install fftw3
+sudo apt-get install glibc-*
+#downlowd the ipConnection apt then
